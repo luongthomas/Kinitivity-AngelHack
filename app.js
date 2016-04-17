2ac1ced45bb0ef87e53d854cddeaeffdfa18bf98
Thomas Luong <Puroof@Thomass-MacBook-Air.local>
2016-04-17 12:35:10 -0700

    Redirected to APIFrame3

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get('/first', function(req,res){
 });
 
 // Redirect
-app.get('/APIFrame2', function(req,res){
+app.get('/APIFrame3', function(req,res){
     res.sendFile(path.join(__dirname + '/APIFrame3.html'));
 });
 
